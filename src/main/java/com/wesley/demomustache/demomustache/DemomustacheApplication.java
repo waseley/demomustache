92789a08451fbb93d18d388a15871b0b75c78327
Wesley Santana <wesley.santana@dataseed.de>
2019-09-20 11:37:35 -0300

    primeiro commit

---
+++ src/main/java/com/wesley/demomustache/demomustache/DemomustacheApplication.java
@@ -0,0 +1,15 @@
+package com.wesley.demomustache.demomustache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomustacheApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DemomustacheApplication.class, args);
+    }
+
+}
